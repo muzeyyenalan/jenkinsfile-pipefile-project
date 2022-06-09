8b407ee9fb98e1c86de1d96f0c8604fa085f77f0
muzeyyen <muzeyyenbas@gmail.com>
2022-06-09 09:25:50 +0300

    hello update

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Javac");
+        System.out.println("Hello from Java to Muzeyyen");
     }
 }
